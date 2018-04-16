fcb61be217c316fffdcab7a07c1e72665a368f29
Jonathan Alpizar <jonalpmo@gmail.com>
2018-04-16 14:47:08 -0600

    First changes

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,6 +4,7 @@ import Helmet from 'react-helmet'
 
 import Header from '../components/header'
 import './index.css'
+import './header.css'
 
 const Layout = ({ children, data }) => (
   <div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,13 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
+    <div>
+       <Link to="/404/">Go to page 404</Link>
+    </div>
+    <div>
+      <Link to="/counter/">Counter</Link>
+    </div>
+    
     <img src="https://slack-imgs.com/?c=1&url=https%3A%2F%2Fres.cloudinary.com%2Fben2020%2Fimage%2Ffetch%2Fq_auto%2Fhttps%3A%2F%2Fd3n8a8pro7vhmx.cloudfront.net%2Fliberty4us%2Fpages%2F37%2Fmeta_images%2Foriginal%2Fdecleration-independence-min.jpg%3F1512420487" alt="" />
   </div>
 )
